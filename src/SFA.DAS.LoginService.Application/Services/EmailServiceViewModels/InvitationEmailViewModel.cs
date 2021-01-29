37d7ef6822c827b8ec1cd4f3241a1c63428664f1
Ben Arnold <ben@arnoldfamily.me.uk>
2021-01-29 14:18:18 +0000

    Use GivenName / FamilyName

--- src/SFA.DAS.LoginService.Application/Services/EmailServiceViewModels/InvitationEmailViewModel.cs
+++ src/SFA.DAS.LoginService.Application/Services/EmailServiceViewModels/InvitationEmailViewModel.cs
@@ -2,6 +2,8 @@ namespace SFA.DAS.LoginService.Application.Services.EmailServiceViewModels
 {
     public class InvitationEmailViewModel : EmailViewModel
     {
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
         public string Contact { get; set; }
         public string OrganisationName { get; set; }
         public string ApprenticeshipName { get; set; }
@@ -9,5 +11,6 @@ namespace SFA.DAS.LoginService.Application.Services.EmailServiceViewModels
         public string ServiceTeam { get; set; }
         public string LoginLink { get; set; }
         public string Inviter { get; set; }
+        public string CreateAccountLink { get; set; }
     }
 }
