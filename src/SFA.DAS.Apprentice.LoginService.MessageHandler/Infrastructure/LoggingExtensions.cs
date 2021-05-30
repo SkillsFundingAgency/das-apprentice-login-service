da2b6ec67140d34ec9a8cb0faae6c3e9ef5f9f39
Ben Arnold <ben@arnoldfamily.me.uk>
2021-05-30 07:29:41 +0100

    Use NServiceBus official Azure Service Bus integration

--- src/SFA.DAS.Apprentice.LoginService.MessageHandler/Infrastructure/LoggingExtensions.cs
+++ src/SFA.DAS.Apprentice.LoginService.MessageHandler/Infrastructure/LoggingExtensions.cs
@@ -1,24 +1,24 @@
-﻿using System.IO;
-using System.Reflection;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using System.IO;
+using System.Reflection;
 
 namespace SFA.DAS.Apprentice.LoginService.MessageHandler.Infrastructure
 {
     internal static class LoggingExtensions
     {
         internal static void ConfigureLogging(this IFunctionsHostBuilder builder)
+            => builder.Services.AddLogging(ConfigureLogging);
+
+        internal static void ConfigureLogging(this ILoggingBuilder logBuilder)
         {
-            builder.Services.AddLogging(logBuilder =>
-            {
-                // all logging is filtered out by default
-                logBuilder.AddFilter("SFA.DAS", LogLevel.Information);
-                var rootDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), ".."));
-                var files = Directory.GetFiles(rootDirectory, "nlog.config", SearchOption.AllDirectories)[0];
-                logBuilder.AddNLog(files);
-            });
+            // all logging is filtered out by defualt
+            logBuilder.AddFilter(typeof(Startup).Namespace, LogLevel.Information);
+            var rootDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, ".."));
+            var files = Directory.GetFiles(rootDirectory, "nlog.config", SearchOption.AllDirectories)[0];
+            logBuilder.AddNLog(files);
         }
     }
 }
---
+++ src/SFA.DAS.Apprentice.LoginService.MessageHandler/Infrastructure/NServiceBus/AzureServiceBusTriggerFunction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using SFA.DAS.Apprentice.LoginService.MessageHandler.Infrastructure.NServiceBus;
+using System.Threading.Tasks;
+
+namespace SFASFA.DAS.Apprentice.LoginService.MessageHandler.Infrastructure.NServiceBus
+{
+    internal class AzureServiceBusTriggerFunction
+    {
+        private const string EndpointName = QueueNames.ApprenticeLoginService;
+        private readonly IFunctionEndpoint endpoint;
+
+        public AzureServiceBusTriggerFunction(IFunctionEndpoint endpoint) => this.endpoint = endpoint;
+
+        [FunctionName("AzureServiceBusTrigger")]
+        public async Task Run(
+            [ServiceBusTrigger(queueName: EndpointName, Connection = "NServiceBusConnectionString")] Message message,
+            ILogger logger,
+            ExecutionContext context)
+        {
+            await endpoint.Process(message, context, logger);
+        }
+    }
+}
