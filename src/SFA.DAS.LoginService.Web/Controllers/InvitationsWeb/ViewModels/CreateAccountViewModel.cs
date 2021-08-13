e65ff62a4ffd2bc01c1e8991c21bf90289fd11a7
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-08-13 14:49:50 +0100

    Add UserAccount page and password to model

---
+++ src/SFA.DAS.LoginService.Web/Controllers/InvitationsWeb/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.Models;
+using SFA.DAS.LoginService.Web.Controllers.ResetPassword.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LoginService.Web.Controllers.InvitationsWeb.ViewModels
+{
+    public class CreateAccountViewModel
+    {
+        [Required(ErrorMessage = "Enter a password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm your password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
