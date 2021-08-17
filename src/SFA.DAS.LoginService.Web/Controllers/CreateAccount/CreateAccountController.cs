832524c02761653cdf271525e711951650cfb1c7
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-08-17 10:26:06 +0100

    Adding controller

---
+++ src/SFA.DAS.LoginService.Web/Controllers/CreateAccount/CreateAccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LoginService.Web.Controllers.CreateAccount
+{
+    public class CreateAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
