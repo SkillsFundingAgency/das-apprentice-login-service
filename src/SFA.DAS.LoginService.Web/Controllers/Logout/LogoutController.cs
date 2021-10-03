f8c5c0b180b56cc3a69618905154da455da141d6
chazzysb <cballagan@yahoo.com>
2021-10-03 10:06:50 +0100

    CS-1096-CookiesSignInRedis-Login

--- src/SFA.DAS.LoginService.Web/Controllers/Logout/LogoutController.cs
+++ src/SFA.DAS.LoginService.Web/Controllers/Logout/LogoutController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.LoginService.Application.BuildLogoutViewModel;
 using SFA.DAS.LoginService.Configuration;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace SFA.DAS.LoginService.Web.Controllers.Logout
 {
@@ -21,7 +23,21 @@ namespace SFA.DAS.LoginService.Web.Controllers.Logout
             await SetViewBagClientIdByLogoutId(logoutId);
 
             var vm = await Mediator.Send(new LogoutRequest {LogoutId = logoutId});
-            return View("Loggedout", vm);
+            var result = View("Loggedout", vm);
+
+            if (HttpContext.Request.Cookies.Count > 0)
+            {
+                var cookies = HttpContext.Request.Cookies.Where(x => x.Key.Contains("Apprenticeships") || x.Key.Contains("AspNetCore")).ToList();
+
+                //cookies.ForEach(x => Response.Cookies.Delete(x.Key));
+                cookies.ForEach(x =>
+                {
+                    var options = new CookieOptions { Expires = System.DateTime.Now.AddDays(-1) };
+                    this.Response.Cookies.Append(x.Key, x.Value, options);
+                });
+            }
+
+            return result;
         }
     }
 }
