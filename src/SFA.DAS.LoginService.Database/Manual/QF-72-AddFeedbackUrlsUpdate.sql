48b6a785660541515440886de24ccb5b9b6d1201
codewolf86 <chris@codewolfconsulting.com>
2022-05-09 10:42:10 +0100

    Sign In Updates

---
+++ src/SFA.DAS.LoginService.Database/Manual/QF-72-AddFeedbackUrlsUpdate.sql
@@ -0,0 +1,48 @@
+﻿
+  /*  QF-72
+  
+  Add the Feedback Application SignIn/SignOut Urls to the Login Application
+  
+  AT:
+  https://feedback.at-aas.apprenticeships.education.gov.uk/signin-oidc
+  https://feedback.at-aas.apprenticeships.education.gov.uk/signout-callback-oidc
+  
+  Prod:
+  https://feedback.my.apprenticeships.education.gov.uk/signin-oidc  
+  https://feedback.my.apprenticeships.education.gov.uk/signout-callback-oidc
+  
+  */
+
+  declare @environment nvarchar(10) =
+	case
+		when DB_NAME() = 'das-at-alogin-db' then 'at'
+		when DB_NAME() = 'das-test-alogin-db' then 'test'
+		when DB_NAME() = 'das-test2-alogin-db' then 'test2'
+		when DB_NAME() = 'das-pp-alogin-db' then 'pp'
+		when DB_NAME() = 'das-prd-alogin-db' then ''
+	else null
+  end;
+
+  if @environment is null
+	throw 60000, 'Database should be one of das-at-alogin-db, das-test-alogin-db, das-test2-alogin-db, das-pp-alogin-db, das-prd-alogin-db', 1;
+
+  
+  declare @newSignInLink nvarchar(120), @newSignOutLink nvarchar(120), @clientId int;
+  
+  if DB_NAME() = 'das-prd-alogin-db'
+    begin
+		select @newSignInLink = 'https://feedback.my.apprenticeships.education.gov.uk/signin-oidc';
+    select @newSignOutLink = 'https://feedback.my.apprenticeships.education.gov.uk/signout-callback-oidc';
+    end
+  else
+    begin
+		select @newSignInLink = 'https://feedback.' + @environment + '-aas.apprenticeships.education.gov.uk/signin-oidc';
+    select @newSignOutLink = 'https://feedback.' + @environment + '-aas.apprenticeships.education.gov.uk/signout-callback-oidc';
+	end
+
+  SELECT @clientId = Id FROM [IdentityServer].[Clients] WHERE ClientId = 'apprentice'
+    
+  INSERT INTO [IdentityServer].[ClientRedirectUris] (RedirectUri, ClientId) VALUES(@newSignInLink, @clientId)
+  INSERT INTO [IdentityServer].[ClientPostLogoutRedirectUris] (PostLogoutRedirectUri, ClientId) VALUES(@newSignOutLink, @clientId)
+  
+
